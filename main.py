032553353492e5a92235d2a05ed2f9b7e4b13ce3
keykomi <keyko999bt@gmail.com>
2023-06-01 16:42:55 +0300

    Add lab

---
+++ main.py
@@ -0,0 +1,20 @@
+import datetime
+
+def calculate_age(born):
+    today = datetime.date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+surname, name, patronymic = input("Введите ФИО через пробел: ").split()
+birth_date = input("Введите дату рождения в формате дд.мм.гггг: ")
+
+birth_date_obj = datetime.datetime.strptime(birth_date, '%d.%m.%Y').date()
+
+age = calculate_age(birth_date_obj)
+
+gender = ""
+if patronymic.endswith(("ич", "евич", "ович")):
+    gender = "мужской"
+else:
+    gender = "женский"
+
+print(surname, name[0] + ".", patronymic[0] + ".", gender, str(age) + " " + ("год" if age % 10 == 1 and age % 100 != 11 else "года" if 2 <= age % 10 <= 4 and not (12 <= age % 100 <= 14) else "лет"))
